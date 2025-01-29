2c9764b544a182b0368f396805f0ba2b61de1ea3
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2025-01-29 23:34:02 +0100

    Move debug scripts

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -114,105 +114,6 @@ PlayersRadioText4:
 	line "#MON CHANNEL…"
 	done
 
-DebugColor:
-	opentext
-	writetext NowEnteringText
-	pause 45
-	special ColorPicker
-	closetext
-	end
-
-NowEnteringText:
-	text "NOW ENTERING..."
-	line "COLOR PICKER MENU."
-	done
-
-DebugSign:
-	opentext
-	; full pokegear
-	setflag ENGINE_POKEGEAR
-	setflag ENGINE_PHONE_CARD
-	setflag ENGINE_MAP_CARD
-	setflag ENGINE_RADIO_CARD
-	setflag ENGINE_EXPN_CARD
-	; pokedex
-	setflag ENGINE_POKEDEX
-	; useful items
-	giveitem BICYCLE
-	giveitem MASTER_BALL, 99
-	giveitem RARE_CANDY, 99
-	giveitem MAX_REPEL, 99
-	giveitem ESCAPE_ROPE, 99
-	giveitem BERRY, 99
-	; useful moves
-	giveitem HM_CUT
-	giveitem HM_FLY
-	giveitem HM_SURF
-	giveitem HM_STRENGTH
-	giveitem HM_FLASH
-	giveitem HM_WHIRLPOOL
-	giveitem HM_WATERFALL
-	giveitem TM_ROCK_SMASH, 99
-	giveitem TM_HEADBUTT, 99
-	; all badges
-	setflag ENGINE_ZEPHYRBADGE
-	setflag ENGINE_HIVEBADGE
-	setflag ENGINE_PLAINBADGE
-	setflag ENGINE_FOGBADGE
-	setflag ENGINE_STORMBADGE
-	setflag ENGINE_MINERALBADGE
-	setflag ENGINE_GLACIERBADGE
-	setflag ENGINE_RISINGBADGE
-	setflag ENGINE_BOULDERBADGE
-	setflag ENGINE_CASCADEBADGE
-	setflag ENGINE_THUNDERBADGE
-	setflag ENGINE_RAINBOWBADGE
-	setflag ENGINE_MARSHBADGE
-	setflag ENGINE_SOULBADGE
-	setflag ENGINE_VOLCANOBADGE
-	setflag ENGINE_EARTHBADGE
-	; fly anywhere
-	setflag ENGINE_FLYPOINT_NEW_BARK
-	setflag ENGINE_FLYPOINT_CHERRYGROVE
-	setflag ENGINE_FLYPOINT_VIOLET
-	setflag ENGINE_FLYPOINT_AZALEA
-	setflag ENGINE_FLYPOINT_GOLDENROD
-	setflag ENGINE_FLYPOINT_ECRUTEAK
-	setflag ENGINE_FLYPOINT_OLIVINE
-	setflag ENGINE_FLYPOINT_CIANWOOD
-	setflag ENGINE_FLYPOINT_MAHOGANY
-	setflag ENGINE_FLYPOINT_LAKE_OF_RAGE
-	setflag ENGINE_FLYPOINT_BLACKTHORN
-	setflag ENGINE_FLYPOINT_SILVER_CAVE
-	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
-	setflag ENGINE_FLYPOINT_PALLET
-	setflag ENGINE_FLYPOINT_VIRIDIAN
-	setflag ENGINE_FLYPOINT_PEWTER
-	setflag ENGINE_FLYPOINT_CERULEAN
-	setflag ENGINE_FLYPOINT_VERMILION
-	setflag ENGINE_FLYPOINT_CELADON
-	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
-	setflag ENGINE_FLYPOINT_LAVENDER
-	setflag ENGINE_FLYPOINT_FUCHSIA
-	setflag ENGINE_FLYPOINT_SAFFRON
-	setflag ENGINE_FLYPOINT_CINNABAR
-	; debug team
-	givepoke FERALIGATR, 100
-	givepoke PIDGEOT, 100
-	givepoke LAPRAS, 100
-	givepoke PIKACHU, 100
-	givepoke JIGGLYPUFF, 100
-	; misc pokemon for testing
-	givepoke PIKACHU, 50
-	givepoke GENGAR, 50
-	givepoke DITTO, 50
-	closetext
-	playsound SFX_WARP_TO
-	special ClearBGPalettesBufferScreen
-	waitsfx
-	warp ROUTE_23, 11, 06
-	end
-
 PlayersHouse2F_MapEvents:
 	db 0, 0 ; filler
 
@@ -226,8 +127,6 @@ PlayersHouse2F_MapEvents:
 	bg_event  3,  1, BGEVENT_READ, PlayersHouseRadioScript
 	bg_event  5,  1, BGEVENT_READ, PlayersHouseBookshelfScript
 	bg_event  6,  0, BGEVENT_IFSET, PlayersHousePosterScript
-	bg_event  0,  0, BGEVENT_READ, DebugColor
-	bg_event  1,  0, BGEVENT_READ, DebugSign
 
 	def_object_events
 	object_event  4,  2, SPRITE_CONSOLE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PlayersHouseGameConsoleScript, EVENT_PLAYERS_HOUSE_2F_CONSOLE
--- maps/RedsHouse2F.asm
+++ maps/RedsHouse2F.asm
@@ -23,6 +23,105 @@ RedsHouse2FPCText:
 	cont "in a long time…"
 	done
 
+DebugColor:
+	opentext
+	writetext NowEnteringText
+	pause 45
+	special ColorPicker
+	closetext
+	end
+
+NowEnteringText:
+	text "NOW ENTERING..."
+	line "COLOR PICKER MENU."
+	done
+
+DebugSign:
+	opentext
+	; full pokegear
+	setflag ENGINE_POKEGEAR
+	setflag ENGINE_PHONE_CARD
+	setflag ENGINE_MAP_CARD
+	setflag ENGINE_RADIO_CARD
+	setflag ENGINE_EXPN_CARD
+	; pokedex
+	setflag ENGINE_POKEDEX
+	; useful items
+	giveitem BICYCLE
+	giveitem MASTER_BALL, 99
+	giveitem RARE_CANDY, 99
+	giveitem MAX_REPEL, 99
+	giveitem ESCAPE_ROPE, 99
+	giveitem BERRY, 99
+	; useful moves
+	giveitem HM_CUT
+	giveitem HM_FLY
+	giveitem HM_SURF
+	giveitem HM_STRENGTH
+	giveitem HM_FLASH
+	giveitem HM_WHIRLPOOL
+	giveitem HM_WATERFALL
+	giveitem TM_ROCK_SMASH, 99
+	giveitem TM_HEADBUTT, 99
+	; all badges
+	setflag ENGINE_ZEPHYRBADGE
+	setflag ENGINE_HIVEBADGE
+	setflag ENGINE_PLAINBADGE
+	setflag ENGINE_FOGBADGE
+	setflag ENGINE_STORMBADGE
+	setflag ENGINE_MINERALBADGE
+	setflag ENGINE_GLACIERBADGE
+	setflag ENGINE_RISINGBADGE
+	setflag ENGINE_BOULDERBADGE
+	setflag ENGINE_CASCADEBADGE
+	setflag ENGINE_THUNDERBADGE
+	setflag ENGINE_RAINBOWBADGE
+	setflag ENGINE_MARSHBADGE
+	setflag ENGINE_SOULBADGE
+	setflag ENGINE_VOLCANOBADGE
+	setflag ENGINE_EARTHBADGE
+	; fly anywhere
+	setflag ENGINE_FLYPOINT_NEW_BARK
+	setflag ENGINE_FLYPOINT_CHERRYGROVE
+	setflag ENGINE_FLYPOINT_VIOLET
+	setflag ENGINE_FLYPOINT_AZALEA
+	setflag ENGINE_FLYPOINT_GOLDENROD
+	setflag ENGINE_FLYPOINT_ECRUTEAK
+	setflag ENGINE_FLYPOINT_OLIVINE
+	setflag ENGINE_FLYPOINT_CIANWOOD
+	setflag ENGINE_FLYPOINT_MAHOGANY
+	setflag ENGINE_FLYPOINT_LAKE_OF_RAGE
+	setflag ENGINE_FLYPOINT_BLACKTHORN
+	setflag ENGINE_FLYPOINT_SILVER_CAVE
+	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
+	setflag ENGINE_FLYPOINT_PALLET
+	setflag ENGINE_FLYPOINT_VIRIDIAN
+	setflag ENGINE_FLYPOINT_PEWTER
+	setflag ENGINE_FLYPOINT_CERULEAN
+	setflag ENGINE_FLYPOINT_VERMILION
+	setflag ENGINE_FLYPOINT_CELADON
+	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
+	setflag ENGINE_FLYPOINT_LAVENDER
+	setflag ENGINE_FLYPOINT_FUCHSIA
+	setflag ENGINE_FLYPOINT_SAFFRON
+	setflag ENGINE_FLYPOINT_CINNABAR
+	; debug team
+	givepoke FERALIGATR, 100
+	givepoke PIDGEOT, 100
+	givepoke LAPRAS, 100
+	givepoke PIKACHU, 100
+	givepoke JIGGLYPUFF, 100
+	; misc pokemon for testing
+	givepoke PIKACHU, 50
+	givepoke GENGAR, 50
+	givepoke DITTO, 50
+	closetext
+	playsound SFX_WARP_TO
+	special ClearBGPalettesBufferScreen
+	waitsfx
+	warp ROUTE_23, 11, 06
+	end
+
 RedsHouse2F_MapEvents:
 	db 0, 0 ; filler
 
@@ -34,5 +133,7 @@ RedsHouse2F_MapEvents:
 	def_bg_events
 	bg_event  3,  5, BGEVENT_READ, RedsHouse2FN64Script
 	bg_event  0,  1, BGEVENT_READ, RedsHouse2FPCScript
+	bg_event  4,  1, BGEVENT_READ, DebugColor
+	bg_event  5,  1, BGEVENT_READ, DebugSign
 
 	def_object_events
