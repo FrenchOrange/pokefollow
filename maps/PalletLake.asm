43e2160f304a5662160059cf5406712f73c410a1
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2025-03-05 23:52:00 +0100

    Update cut tree sprite

--- maps/PalletLake.asm
+++ maps/PalletLake.asm
@@ -1,8 +1,9 @@
 	object_const_def
-	const ROUTE1_MISTY_FISH
-	const ROUTE1_MISTY_ROD
-	const ROUTE1_MISTY
-	const ROUTE1_BIKE
+	const PALLETLAKE_MISTY_FISH
+	const PALLETLAKE_MISTY_ROD
+	const PALLETLAKE_MISTY
+	const PALLETLAKE_BIKE
+	const PALLETLAKE_POKE_BALL1
 
 PalletLake_MapScripts:
 	def_scene_scripts
@@ -23,11 +24,11 @@ PalletLake_MapScripts:
 	; move down
 	; exclamation Misty
 	; big catch text
-	; disappear ROUTE1_MISTY_ROD
+	; disappear PALLETLAKE_MISTY_ROD
 	; jump movement
-	; turnobject ROUTE1_MISTY_FISH, DOWN
-	; appear ROUTE1_MISTY
-	; disappear ROUTE1_MISTY_FISH
+	; turnobject PALLETLAKE_MISTY_FISH, DOWN
+	; appear PALLETLAKE_MISTY
+	; disappear PALLETLAKE_MISTY_FISH
 	; Misty consternation.
 	end
 
@@ -38,6 +39,9 @@ PalletLakeHideObjectMovement:
 PalletLakeShowObjectMovement:
 	show_object
 	step_end
+
+PalletLakeRepel:
+	itemball REPEL
 
 PalletLake_MapEvents:
 	db 0, 0 ; filler
@@ -53,3 +57,4 @@ PalletLake_MapEvents:
 	object_event  8, 17, SPRITE_MISTY_FISH, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MET_MISTY_PALLET_LAKE
 	object_event  8, 18, SPRITE_MISTY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_INVISIBLE_NPC
 	object_event  6, 21, SPRITE_BIKE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MET_MISTY_PALLET_LAKE
+	object_event 19,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PalletLakeRepel, EVENT_PALLET_LAKE_REPEL
